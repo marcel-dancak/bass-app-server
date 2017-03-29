87a7ff6b88c594353fd9c467cd23c84dd1cebbcd
Marcel Dancak <dancakm@gmail.com>
2017-03-29 19:12:00 +0200

    Server: added support to reset user's password

--- server/bassapp/accounts/urls.py
+++ server/bassapp/accounts/urls.py
@@ -1,12 +1,27 @@
 from django.conf.urls import url, include
+from django.contrib.auth.views import password_reset, password_reset_confirm, password_reset_complete
 
-from .views import SpaRegistrationView
+from .views import RegistrationView, ResetPassword
 
 
 urlpatterns = [
-    url(r'register/$',
-        SpaRegistrationView.as_view(),
-        name='registration_register',
+    url(r'^register/$',
+        RegistrationView.as_view(),
+        name='registration_register'
+    ),
+    url(r'^password_reset/$',
+        ResetPassword.as_view(),
+        name='password_reset'
+    ),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        password_reset_confirm,
+        {'template_name': 'accounts/password_reset_confirm.html'},
+        name='password_reset_confirm'
+    ),
+    url(r'^reset/done/$',
+        password_reset_complete,
+        {'template_name': 'accounts/password_reset_complete.html'},
+        name='password_reset_complete'
     ),
     url(r'', include('registration.backends.hmac.urls')),
 ]
--- server/bassapp/accounts/views.py
+++ server/bassapp/accounts/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.tokens import default_token_generator
 
-from registration.backends.hmac.views import RegistrationView
+from registration.backends.hmac.views import RegistrationView as HmacRegistrationView
+
 from .forms import UserRegistrationForm
 
 
-class SpaRegistrationView(RegistrationView):
+class RegistrationView(HmacRegistrationView):
     form_class = UserRegistrationForm
 
     def form_valid(self, form):
@@ -16,4 +20,26 @@ class SpaRegistrationView(RegistrationView):
             form.errors.as_json(),
             content_type='application/json',
             status=409
+        )
+
+
+class ResetPassword(FormView):
+    form_class = PasswordResetForm
+
+    def form_valid(self, form):
+        opts = {
+            'use_https': self.request.is_secure(),
+            'token_generator': default_token_generator,
+            'email_template_name': 'accounts/password_reset_email.txt',
+            'subject_template_name': 'accounts/password_reset_email_subject.txt',
+            'request': self.request,
+        }
+        form.save(**opts)
+        return HttpResponse("ko")
+
+    def form_invalid(self, form):
+        return HttpResponse(
+            form.errors.as_json(),
+            content_type='application/json',
+            status=400
         )
