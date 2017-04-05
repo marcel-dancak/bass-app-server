0fe945045f7f5f14e266f97647f14a9287a514d6
Marcel Dancak <dancakm@gmail.com>
2017-04-05 20:15:09 +0200

    Server: updated import

--- server/bassapp/accounts/urls.py
+++ server/bassapp/accounts/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib.auth.views import password_reset, password_reset_confirm, password_reset_complete
 
-from .views import RegistrationView, ResetPassword
+from . import views
 
 
 urlpatterns = [
     url(r'^register/$',
-        RegistrationView.as_view(),
+        views.RegistrationView.as_view(),
         name='registration_register'
     ),
     url(r'^password_reset/$',
-        ResetPassword.as_view(),
+        views.ResetPassword.as_view(),
         name='password_reset'
     ),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
@@ -23,5 +23,5 @@ urlpatterns = [
         {'template_name': 'accounts/password_reset_complete.html'},
         name='password_reset_complete'
     ),
-    url(r'', include('registration.backends.hmac.urls')),
+    url(r'', include('registration.backends.hmac.urls'))
 ]
