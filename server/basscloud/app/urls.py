bf31737e3f877fff8aa5156cc1f2b103b6523199
Marcel Dancak <dancakm@gmail.com>
2017-09-13 23:53:51 +0200

    Server: added youtube-dl tool for fetching online streams info

--- server/basscloud/app/urls.py
+++ server/basscloud/app/urls.py
@@ -6,5 +6,6 @@ from basscloud.decorators import view_cache
 
 urlpatterns = [
     url(r"^app$", views.app, name="app"),
-    url(r"^app/(\w+)(.json/)?$", view_cache(timeout=60*5)(views.app_data), name="project_data")
+    url(r"^app/(\w+)(.json/)?$", view_cache(timeout=60*5)(views.app_data), name="project_data"),
+    url(r"^api/online_stream/$", views.extract_streams)
 ]
--- server/basscloud/app/views.py
+++ server/basscloud/app/views.py
@@ -15,3 +15,45 @@ def app(request):
 def app_data(request, id, *args):
     project = get_object_or_404(Project, pk=id)
     return HttpResponse(project.data, content_type='text/plain')
+
+
+class YtLogger(object):
+    output = ''
+    url = ''
+    json = ''
+    def debug(self, msg):
+        self.output += msg+'\n'
+        if msg.startswith('https://'):
+            self.url = msg
+        if msg.startswith('{"'):
+            self.json = msg
+
+    def warning(self, msg):
+        print(msg)
+
+    def error(self, msg):
+        print(msg)
+
+def extract_streams(request):
+    # link = 'https://www.youtube.com/watch?v='+request.GET['id']
+    link = request.GET['url']
+    import youtube_dl
+    # youtube_dl._real_main(['-f', '250', '-g', link])
+
+    logger = YtLogger()
+    ydl_opts = {
+        'quiet': True,
+        'simulate': True,
+        'forcejson': True,
+        # 'forceurl': True,
+        # 'format': '250',
+        'logger': logger,
+        'verbose': False
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+        # print(logger.output)
+
+    return HttpResponse(logger.json, content_type='application/json')
+    return HttpResponse(logger.url, content_type='text/plain')
